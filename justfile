ef8f42597ed5944d98c05f08be332070562d75a9
Gary Woodfine <gary.woodfine@threenine.co.uk>
2025-01-29 08:41:09 +0000

    Add output option to save profile data to a file
    This update introduces an `--output` flag to the `profile` command, enabling the user to save the fetched profile data to a JSON file. The implementation includes writing the profile to a file named after the provided `npub` and handling potential errors during the write operation.

--- justfile
+++ justfile
@@ -5,4 +5,4 @@ build:
   chmod +x dist/app.js
 
 test-profile: build
-    ./dist/app.js profile --npub npub1a3jz7w9laar2k77dkkh2rhxunr9zpja5emrl5yrxetamt29swqyqmmt32y
+    ./dist/app.js profile --npub npub1a3jz7w9laar2k77dkkh2rhxunr9zpja5emrl5yrxetamt29swqyqmmt32y --output
--- src/commands/profile.ts
+++ src/commands/profile.ts
@@ -1,6 +1,9 @@
 import {CommandModule} from 'yargs';
 import {command} from "./command";
 import {NdkService} from "../services/NdkService";
+import {promises as fs} from 'fs'
+import {NDKUserProfile} from "@nostr-dev-kit/ndk";
+import * as process from "node:process";
 
 const profileCommand: CommandModule = {
     command: command.profile,
@@ -11,30 +14,43 @@ const profileCommand: CommandModule = {
             demandOption: true,
             type: 'string',
         },
-        age: {
-            describe: 'Age of the user',
+        output: {
+            describe: 'should write to file',
             demandOption: false,
-            type: 'number',
+            type: 'boolean',
+            default: false
         },
     },
     handler: async (argv) => {
         if (argv.npub) {
-            console.log("npub: ", argv.npub)
-            await getByPubkey(argv.npub.toString())
+            let profile = await getProfile(argv.npub.toString())
+            console.log(profile)
+            if (argv.output && profile !== undefined) {
+                let result = await writeToFile(argv.npub.toString(), profile)
+                if(result.success) {
+                    console.log(`output to file: ${result.fileName}`)
+                } else {
+                    console.log(`Failed to write profile to ${result.fileName}`)
+                    process.exit(7)
+                }
+            }
         }
+        process.exit(0);
     },
 };
 
-async function getByPubkey(npub: string): Promise<void> {
+async function writeToFile(npub: string, profile: NDKUserProfile): Promise<{ success: Boolean, fileName: string }> {
+
+    let outputFilePath = `./${npub}.json`;
+    await fs.writeFile(outputFilePath, JSON.stringify(profile, null, 2), 'utf-8');
+    return {success: true, fileName: outputFilePath}
+}
+
+async function getProfile(npub: string): Promise<NDKUserProfile | undefined> {
     let ndk = await NdkService.getInstance().getNDK()
-
-
-    const user = ndk.getUser({ npub: npub as string});
-
+    const user = ndk.getUser({npub: npub as string});
     await user.fetchProfile()
-    console.log(user.profile)
-    process.exit(0);
-
+    return user.profile
 }
 
 export default profileCommand;
